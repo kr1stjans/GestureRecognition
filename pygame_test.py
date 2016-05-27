3f2f4109e055930c19ed209e0d499039c879ef04
Ernest Beličič <ernest.belicic@gmail.com>
2016-05-27 12:38:51 +0200

    pygame test
    pygame test program for windows

--- pygame_test.py
+++ pygame_test.py
@@ -1,10 +1,6 @@
 import pygame
 
-if __name__ == "__main__":
-
-    pygame.init()
-    sound = pygame.mixer.Sound("pitch_sounds/bass_d.wav")
-    sound.play()
-
-    while True:
-        a = 1
+pygame.mixer.init()
+sound = pygame.mixer.Sound("pitch_sounds/chord_bm.wav")
+sound.play()
+pygame.time.delay(2000)
